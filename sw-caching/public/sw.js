3dc9450b321462c14ec681aab5f7301e692ff45d
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2022-07-05 15:54:58 +0530

    Handling Errors

--- sw-caching/public/sw.js
+++ sw-caching/public/sw.js
@@ -40,7 +40,9 @@ self.addEventListener("fetch", function (event) {
             cache.put(event.request.url, res.clone());
             return res;
           });
-        });
+        }).catch((err)=>{
+          
+        })
       }
     })
   );
