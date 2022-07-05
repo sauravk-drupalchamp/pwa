a0d96cee2e25cb3eadb58007dafcfef4e5e28a05
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2022-07-05 10:49:24 +0530

    Promise, Fetch, Ajax,  Polyfill

--- pwa-app/public/src/js/app.js
+++ pwa-app/public/src/js/app.js
@@ -1,4 +1,8 @@
 var defferedPrompt;
+if(!window.Promise){
+  window.Promise = Promise;
+}
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js").then(function () {
     console.log("Service worker registered!");
@@ -20,6 +24,48 @@ var promise = new Promise(function(resolve, reject){
     reject({code: 500,message: 'An error occured'})
     // console.log("This is executed after timer is done")
   },3000)
+})
+
+// AJAX
+var xhr = new XMLHttpRequest();
+xhr.open('GET','https://httpbin.org/ip');
+xhr.responseType = 'json';
+
+xhr.onload = function (){
+  console.log(xhr.response)
+}
+
+xhr.onerror = function (){
+  console.log('Error!')
+}
+
+xhr.send();
+
+fetch('https://httpbin.org/ip').then((res)=>{
+  console.log(res,"RESPONSE")
+  return res.json()
+}).then((data)=>{
+  console.log(data)
+}).catch((err)=>{
+  console.log(err)
+})
+
+fetch('https://httpbin.org/post',
+{
+  method: 'POST',
+  headers: {
+    "Content-Type" : "application/json",
+    "Accept" : "application/json"
+  },
+  mode: 'cors',
+  body: JSON.stringify({message: "Does this works !"})
+}).then((res)=>{
+  console.log(res,"RESPONSE")
+  return res.json()
+}).then((data)=>{
+  console.log(data)
+}).catch((err)=>{
+  console.log(err)
 })
 
 // promise.then(function(text){
