9e1dc4bac2e77d462a968e3556c12b076ef375df
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2022-07-08 16:10:57 +0530

    Added comment for CACHE only

--- adv-caching-01/public/sw.js
+++ adv-caching-01/public/sw.js
@@ -1,26 +1,27 @@
-var CACHE_STATIC_NAME = "static-v19";
-var CACHE_DYNAMIC_NAME = "dynamic-v19";
+var CACHE_STATIC_NAME = "static-v22";
+var CACHE_DYNAMIC_NAME = "dynamic-v22";
+var STATIC_FILES_NAME = [
+  "/",
+  "/index.html",
+  "/offline.html",
+  "/src/js/app.js",
+  "/src/js/feed.js",
+  "/src/js/promise.js",
+  "/src/js/fetch.js",
+  "/src/js/material.min.js",
+  "/src/css/app.css",
+  "/src/css/feed.css",
+  "/src/images/main-image.jpg",
+  "https://fonts.googleapis.com/css?family=Roboto:400,700",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+];
 self.addEventListener("install", function (event) {
   console.log("[Service Worker] Installing Service Worker ...", event);
   event.waitUntil(
     caches.open(CACHE_STATIC_NAME).then(function (cache) {
       console.log("[Service Worker] Precaching App Shell");
-      cache.addAll([
-        "/",
-        "/index.html",
-        "/offline.html",
-        "/src/js/app.js",
-        "/src/js/feed.js",
-        "/src/js/promise.js",
-        "/src/js/fetch.js",
-        "/src/js/material.min.js",
-        "/src/css/app.css",
-        "/src/css/feed.css",
-        "/src/images/main-image.jpg",
-        "https://fonts.googleapis.com/css?family=Roboto:400,700",
-        "https://fonts.googleapis.com/icon?family=Material+Icons",
-        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
-      ]);
+      cache.addAll(STATIC_FILES_NAME);
     })
   );
 });
@@ -113,7 +114,14 @@ self.addEventListener('fetch', function (event) {
             });
         })
     );
-  } else {
+  } 
+  // CACHE ONLY
+  // else if (new RegExp('\\b' + STATIC_FILES_NAME.join('\\b|\\b') + '\\b').test(event.request.url)) {
+  //   event.respondWith(
+  //     caches.match(event.request)
+  //   );
+  // } 
+  else {
     event.respondWith(
       caches.match(event.request)
         .then(function (response) {
