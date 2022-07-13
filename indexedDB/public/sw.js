0f4aed8e84947c1a99caa41dc8653b51a43cfb44
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2022-07-13 13:51:56 +0530

    Storing Fetched Data in indexedDB

--- indexedDB/public/sw.js
+++ indexedDB/public/sw.js
@@ -1,6 +1,7 @@
+// Imported IDB file and Added To static Cache
 importScripts('/src/js/idb.js')
-var CACHE_STATIC_NAME = 'static-v18';
-var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+var CACHE_STATIC_NAME = 'static-v20';
+var CACHE_DYNAMIC_NAME = 'dynamic-v20';
 var STATIC_FILES = [
   '/',
   '/index.html',
@@ -18,6 +19,14 @@ var STATIC_FILES = [
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
+
+// Variable used to make connection with DB
+var dbPromise = idb.open('posts-store', 1, function(db){
+  if(!db.objectStoreNames.contains('posts')){
+    // It creates new Object Store that can be accessed by dbPromise.
+    db.createObjectStore('posts', {keyPath: 'id'})
+  }
+})
 
 // function trimCache(cacheName, maxItems) {
 //   caches.open(cacheName)
@@ -69,19 +78,26 @@ function isInArray(string, array) {
   }
   return array.indexOf(cachePath) > -1;
 }
+
 self.addEventListener('fetch', function (event) {
 
   var url = 'https://pwagram-f9e3c-default-rtdb.firebaseio.com/posts';
   if (event.request.url.indexOf(url) > -1) {
     event.respondWith(
-      caches.open(CACHE_DYNAMIC_NAME)
-        .then(function (cache) {
-          return fetch(event.request)
-            .then(function (res) {
-              // trimCache(CACHE_DYNAMIC_NAME, 3);
-              cache.put(event.request, res.clone());
-              return res;
-            });
+      fetch(event.request)
+        .then(function (res){
+          var clonedResponse = res.clone();
+          clonedResponse.json().then(function(data){
+            for(var key in data){
+              dbPromise.then(function(db){
+                var tx = db.transaction('posts','readwrite');
+                var store = tx.objectStore('posts');
+                store.put(data[key]);
+                return tx.complete;
+              })
+            }
+          })
+          return res; 
         })
     );
   } else if (isInArray(event.request.url, STATIC_FILES)) {
