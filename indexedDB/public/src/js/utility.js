b4d4627cc41d01c34e5c6822f08be5a52b2c0d64
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2022-07-13 16:02:05 +0530

    Added utility.js

---
+++ indexedDB/public/src/js/utility.js
@@ -0,0 +1,15 @@
+var dbPromise = idb.open("posts-store", 1, function (db) {
+  if (!db.objectStoreNames.contains("posts")) {
+    // It creates new Object Store that can be accessed by dbPromise.
+    db.createObjectStore("posts", { keyPath: "id" });
+  }
+});
+
+function writeData(st, data) {
+  return dbPromise.then(function (db) {
+    var tx = db.transaction(st, "readwrite");
+    var store = tx.objectStore(st);
+    store.put(data);
+    return tx.complete;
+  });
+}
--- indexedDB/public/sw.js
+++ indexedDB/public/sw.js
@@ -1,5 +1,6 @@
 // Imported IDB file and Added To static Cache
 importScripts('/src/js/idb.js')
+importScripts('src/js/utility.js')
 var CACHE_STATIC_NAME = 'static-v20';
 var CACHE_DYNAMIC_NAME = 'dynamic-v20';
 var STATIC_FILES = [
@@ -20,13 +21,13 @@ var STATIC_FILES = [
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
-// Variable used to make connection with DB
-var dbPromise = idb.open('posts-store', 1, function(db){
-  if(!db.objectStoreNames.contains('posts')){
-    // It creates new Object Store that can be accessed by dbPromise.
-    db.createObjectStore('posts', {keyPath: 'id'})
-  }
-})
+// Variable used to make connection with DB == CODE SHIFTED IN UTILITY.JS
+// var dbPromise = idb.open('posts-store', 1, function(db){
+//   if(!db.objectStoreNames.contains('posts')){
+//     // It creates new Object Store that can be accessed by dbPromise.
+//     db.createObjectStore('posts', {keyPath: 'id'})
+//   }
+// })
 
 // function trimCache(cacheName, maxItems) {
 //   caches.open(cacheName)
@@ -89,12 +90,7 @@ self.addEventListener('fetch', function (event) {
           var clonedResponse = res.clone();
           clonedResponse.json().then(function(data){
             for(var key in data){
-              dbPromise.then(function(db){
-                var tx = db.transaction('posts','readwrite');
-                var store = tx.objectStore('posts');
-                store.put(data[key]);
-                return tx.complete;
-              })
+              writeData(('posts', data[key]))
             }
           })
           return res; 
